89de6148672aa38a21f97c9f13d9dec6a6e3db71
jocoly <62028785+jocoly@users.noreply.github.com>
2022-03-09 14:35:57 -0500

    Overhaul

---
+++ bot/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+//variables used for command line parameters
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+const testMessage = "If you're reading this, the bot works."
+
+func main() {
+	//create a new Discord session using the provided bot token
+	dg, err := discordgo.new("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	//register the messageCreate function as a callback for MessageCreate events
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	//open a websocket connection to Discord and begin listening
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	//everything runs here until one of the term signals is received
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	//close the Discord session
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	//ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID{
+		return
+	}
+
+	if m.Content == "Does the bot work?"{
+		s.ChennelMessageSend(m.ChannelID, testMessage)
+	} else{
+		return
+	}
+	}
+}
